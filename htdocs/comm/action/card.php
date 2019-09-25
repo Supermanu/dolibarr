0e4649dac0b81f7bb6cf28be1f76d5fe664eef7b
Manuel Tondeur <manueltondeur@gmail.com>
2019-09-25 12:33:00 +0200

    Require end date for events

--- htdocs/comm/action/card.php
+++ htdocs/comm/action/card.php
@@ -321,7 +321,7 @@ if ($action == 'add')
 		$action = 'create';
 		setEventMessages($langs->trans("ErrorFieldRequired", $langs->transnoentitiesnoconv("ActionsOwnedBy")), null, 'errors');
 	}
-	if ($object->type_code == 'AC_RDV' && ($datep == '' || ($datef == '' && empty($fulldayevent))))
+	if (($datep == '' || ($datef == '' && empty($fulldayevent))))
 	{
 		$error++; $donotclearsession=1;
 		$action = 'create';
@@ -673,10 +673,6 @@ if ($action == 'create')
                         {
                             $("#doneby").val(-1);
                         }
-                   });
-                   $("#actioncode").change(function() {
-                        if ($("#actioncode").val() == \'AC_RDV\') $("#dateend").addClass("fieldrequired");
-                        else $("#dateend").removeClass("fieldrequired");
                    });
                })';
         print '</script>'."\n";

7cacfd9d292fc0e260e52ab80f569b08cd8ade7e
Supermanu <manueltondeur@gmail.com>
2022-06-24 12:57:10 +0200

    NEW Enable filtering for multiple users at once for calendar view

--- htdocs/core/lib/agenda.lib.php
+++ htdocs/core/lib/agenda.lib.php
@@ -84,7 +84,7 @@ function print_actions_filter($form, $canedit, $status, $year, $month, $day, $sh
 		// Assigned to user
 		print '<div class="divsearchfield">';
 		print img_picto($langs->trans("ActionsToDoBy"), 'user', 'class="fawidth30 inline-block"');
-		print $form->select_dolusers($filtert, 'search_filtert', 1, '', !$canedit, '', '', 0, 0, 0, '', 0, '', 'minwidth150 maxwidth500 widthcentpercentminusxx');
+		print $form->select_dolusers($filtert, 'search_filtert', 1, '', !$canedit, '', '', 0, 0, 0, '', 0, '', 'maxwidth500 widthcentpercentminusxx', 0, 0, true);
 		print '</div>';
 
 		// Assigned to user group

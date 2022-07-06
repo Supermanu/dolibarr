0d6977772d7f4074858f87688ffebca602782028
Supermanu <manueltondeur@gmail.com>
2022-07-06 16:05:12 +0200

    Toggle all events in month view

--- htdocs/comm/action/index.php
+++ htdocs/comm/action/index.php
@@ -560,6 +560,9 @@ if (!empty($conf->use_javascript_ajax)) {	// If javascript on
 		$s .= 'frm.attr("action", newurl).children("#newdate").val(newval);frm.submit();}'."\n";
 		$s .= '});'."\n";
 	}
+	if ($action == "show_month") {
+		$s .= 'jQuery(".check_showallevents").click(function() { for (let i = 0; i < $("div[id^=more_]").length; i++) {$("div[id^=more_]")[i].click()}});';
+	}
 	$s .= '});'."\n";
 	$s .= '</script>'."\n";
 
@@ -588,6 +591,9 @@ if (!empty($conf->use_javascript_ajax)) {	// If javascript on
 
 	// Birthdays
 	$s .= '<div class="nowrap inline-block"><input type="checkbox" id="check_birthday" name="check_birthday" class="check_birthday"><label for="check_birthday"> <span class="check_birthday_text">'.$langs->trans("AgendaShowBirthdayEvents").'</span></label> &nbsp; </div>';
+	if ($action == "show_month") {
+		$s .= '<div class="nowrap inline-block"><input type="checkbox" id="check_showallevents" name="check_showallevents" class="check_showallevents"><label for="check_showallevents"> <span class="check_showallevents_text">Montrer tous les événements</span></label> &nbsp; </div>';
+	}
 
 	// Calendars from hooks
 	$parameters = array(); $object = null;
